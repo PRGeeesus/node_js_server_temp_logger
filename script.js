c68d382c2ea2684cb9cb5938511f5272017f7201
PRGeeesus <46870324+PRGeeesus@users.noreply.github.com>
2019-12-06 18:49:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+let temp = 0
+while(1):
+  console.log("Server running at http:");
+  console.log(temp);
+  tmpe = temp+1
