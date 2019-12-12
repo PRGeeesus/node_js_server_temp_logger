e83510c7233ad154d03a132d6565f068be829ab3
PRGeeesus <46870324+PRGeeesus@users.noreply.github.com>
2019-12-12 22:24:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const express = require('express')
+var ip = require("ip");
 const app = express()
 const port = 3000
 var temp
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
+
 app.use(function (req, res, next) {
   /*var err = new Error('Not Found');
    err.status = 404;
@@ -17,7 +14,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'esp_simulator_hmtl.html',
                                                'file:///C:/localhost:3000/incomeing',
                                                'https://martintestlogger.000webhostapp.com/esp_simulator_hmtl.html',
-                                               'https://martintestlogger.000webhostapp.com');
+                                               'https://martintestlogger.000webhostapp.com/');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -32,25 +29,27 @@ app.use(function (req, res, next) {
 });
 app.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('something bad happened - this was send by the node server', err)
 }
 
 app.get('/', (request, response) => {
-  response.send("Hello from Express! with acesses")
+  response.send("get request to / - this was send by the node server")
 })
 app.get('/home', (request, response) => {
-  response.send("This is supposed to be the Home Site")
+  response.send("get request to /home - this was send by the node server")
   console.log("Request to /home")
   //console.log(request)
 })
 
 app.post('/incomeing', function(request, response){
-  console.log("Post to /incomeing")
+  console.log("Post to /incomeing - this was send to the node server logging body")
   console.log(request.body)
-
     //console.log(request);
 })
 
 
-  console.log(`server is listening on ${port}`)
+console.log(`server is listening on ${port} at IP:`)
+console.log ( ip.address() );
+console.dir ( ip.address() );
+
 })
